c2c4d459d759df29c81bd6dc74be9ecb57a98043
Ali Atallaoui <ali.attalaoui@gmail.com>
2026-03-10 19:47:01 +0100

    fix(react): import useContext and AuthContext in ProjectContext

--- frontend/src/context/ProjectContext.jsx
+++ frontend/src/context/ProjectContext.jsx
@@ -1,4 +1,5 @@
-import React, { createContext, useState, useEffect } from 'react';
+import React, { createContext, useState, useEffect, useContext } from 'react';
+import { AuthContext } from './AuthContext';
 
 export const ProjectContext = createContext();
 
